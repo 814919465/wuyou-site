7d1a5bdc92db0556e126a26e4030323ca4a2d3d2
814919465@qq.com <harry123>
2019-12-11 22:08:11 +0800

    初始提交

--- src/main/java/com/wuyou/wysite/controller/TestController.java
+++ src/main/java/com/wuyou/wysite/controller/TestController.java
@@ -3,6 +3,8 @@ package com.wuyou.wysite.controller;
 import com.wuyou.wysite.service.ProblemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 心理测试
@@ -14,4 +16,8 @@ public class TestController {
     @Autowired
     ProblemService problemService;
 
+    @GetMapping("/add")
+    public ModelAndView add(){
+        return new ModelAndView("/add");
+    }
 }
